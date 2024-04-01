c34c517ddd662ff28e8b7fc3404dffa13cc2c735
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 19:53:53 -0300

    feat: modify userId in model to number

--- src/repository/model/user.model.ts
+++ src/repository/model/user.model.ts
@@ -2,38 +2,15 @@ import * as mongoose from 'mongoose';
 import { Prop, Ref, getModelForClass, pre } from '@typegoose/typegoose';
 import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import ObjectId = mongoose.Types.ObjectId;
-import { Region } from './region.model';
-import { GeocodeService } from './../../service';
-import { object } from 'joi';
-
-const lib = new GeocodeService();
 
 class Base extends TimeStamps {
     @Prop({ required: true, default: () => new ObjectId().toString() })
     _id: string;
 }
 
-/* @pre<User>('save', async function (next) {
-    const region = this as Omit<any, keyof User> & User;
-
-    if (region.isModified('coordinates')) {
-        region.address = await lib.getAddressFromCoordinates(
-            region.coordinates
-        );
-    } else if (region.isModified('address')) {
-        const { lat, lng } = await lib.getCoordinatesFromAddress(
-            region.address
-        );
-
-        region.coordinates = [lng, lat];
-    }
-
-    next();
-}) */
-
 export class User extends Base {
     @Prop({ required: true })
-    userId!: string;
+    userId!: number;
 
     @Prop({ required: true })
     name!: string;
@@ -46,14 +23,6 @@ export class User extends Base {
 
     @Prop({ required: true })
     coordinates: { lat: number; lng: number };
-
-    /* @Prop({
-        required: true,
-        default: [],
-        ref: () => Region,
-        type: () => String,
-    })
-    regions: Ref<Region>[]; */
 }
 
 export const UserModel = getModelForClass(User);
