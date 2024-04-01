45b07102ef3dc9e028502659f7397ab72f6e8d6f
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 14:40:55 -0300

    feat: create user constants

---
+++ src/constant/user.constant.ts
@@ -0,0 +1,15 @@
+export class UserSuccessMessagesConstants {
+    static readonly CREATED = 'User created successfully!';
+
+    static readonly FOUND = 'User found successfully!';
+
+    static readonly UPDATED = 'User updated successfully!';
+
+    static readonly DELETED = 'User deleted successfully!';
+
+    static readonly FOUND_LIST = 'Users found successfully!';
+}
+
+export class UserErrorMessagesConstants {
+    static readonly NOT_FOUND = 'User not found!';
+}
