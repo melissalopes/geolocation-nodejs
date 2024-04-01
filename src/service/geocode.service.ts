a03aa4eff2f91387c7539499cf21d18f07063716
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 14:38:57 -0300

    feat: create geocode service

---
+++ src/service/geocode.service.ts
@@ -0,0 +1,53 @@
+import { Client } from '@googlemaps/google-maps-services-js';
+import { GeocodeEnvs } from '../config/envs';
+
+export class GeocodeService {
+    private clientMaps: Client;
+
+    constructor() {
+        this.clientMaps = new Client();
+    }
+
+    async getCoordinatesFromAddress(
+        address: string
+    ): Promise<{ lat: number; lng: number }> {
+        try {
+            const response = await this.clientMaps.geocode({
+                params: {
+                    address,
+                    key: GeocodeEnvs.API_KEY,
+                },
+            });
+
+            return response.data.results[0].geometry.location;
+        } catch (error) {
+            console.log(
+                'Error in GeocodeService.getCoordinatesFromAddress',
+                error
+            );
+            throw error;
+        }
+    }
+
+    async getAddressFromCoordinates(latlng: {
+        lat: number;
+        lng: number;
+    }): Promise<string> {
+        try {
+            const response = await this.clientMaps.reverseGeocode({
+                params: {
+                    latlng,
+                    key: GeocodeEnvs.API_KEY,
+                },
+            });
+
+            return response.data.results[0].formatted_address;
+        } catch (error) {
+            console.log(
+                'Error in GeocodeService.getAddressFromCoordinates',
+                error
+            );
+            throw error;
+        }
+    }
+}
