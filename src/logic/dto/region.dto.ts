6b738d85a1030572f61534fa1b73eadc9b742bcf
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 19:56:07 -0300

    feat: create region dto

---
+++ src/logic/dto/region.dto.ts
@@ -0,0 +1,13 @@
+interface RegionDTO {
+    regionId: string;
+    userId: string;
+    name: string;
+}
+export interface RegionReqDTO extends RegionDTO {}
+
+export interface RegionRespDTO extends RegionDTO {}
+
+export interface RegionUpdateReqDTO {
+    id: string;
+    userId: string;
+}
