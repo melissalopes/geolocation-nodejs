44260416dce5d1e89624d1991472502bd9077594
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 19:48:51 -0300

    feat: create a region schema to check if the region data is correctly

---
+++ src/validator/region.schema.ts
@@ -0,0 +1,27 @@
+import * as Joi from 'joi';
+
+export const CreateRegionSchema = Joi.object({
+    regionId: Joi.number().required(),
+    name: Joi.string().required(),
+    userId: Joi.number().required(),
+}).required();
+
+export const GetRegionSchema = Joi.object({
+    id: Joi.string().required(),
+    regionId: Joi.string().required(),
+}).required();
+
+export const UpdateRegionSchema = Joi.object({
+    id: Joi.string().required(),
+}).required();
+
+export const DeleteRegionSchema = Joi.object({
+    id: Joi.string().required(),
+}).required();
+
+export const RegionSchema = {
+    CREATE: CreateRegionSchema,
+    GET: GetRegionSchema,
+    UPDATE: UpdateRegionSchema,
+    DELETE: DeleteRegionSchema,
+};
