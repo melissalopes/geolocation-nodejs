1207408c09eec63f51c6930e8da03c821076732b
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-01 19:47:09 -0300

    feat: create region constants

---
+++ src/constant/region.constant.ts
@@ -0,0 +1,15 @@
+export class RegionSuccessMessagesConstants {
+    static readonly CREATED = 'Region created successfully!';
+
+    static readonly FOUND = 'Region found successfully!';
+
+    static readonly UPDATED = 'Region updated successfully!';
+
+    static readonly DELETED = 'Region deleted successfully!';
+
+    static readonly FOUND_LIST = 'Regions found successfully!';
+}
+
+export class RegionErrorMessagesConstants {
+    static readonly NOT_FOUND = 'Region not found!';
+}
